e913d7ce0f29865ea9382c649f23e74dc2b676fd
singh2236 <nav.kang4@gmail.com>
2022-10-03 21:06:55 +0200

    Custom Security Configurations for public pages & CSRF Disable

--- Example_28/src/main/java/com/navi/modelSchool/config/ProjectSecurityConfig.java
+++ Example_28/src/main/java/com/navi/modelSchool/config/ProjectSecurityConfig.java
@@ -8,19 +8,15 @@ import org.springframework.stereotype.Controller;
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
-        //Permit all the requests inside the web-application
-
         http.authorizeRequests()
-                .anyRequest().permitAll()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/about").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
     }
-/*protected void configure(HttpSecurity http) throws Exception {
-        //Permit all the requests inside the web-application
 
-        http.authorizeRequests()
-                .anyRequest().denyAll()
-                .and().formLogin()
-                .and().httpBasic();
-    }*/
 }
