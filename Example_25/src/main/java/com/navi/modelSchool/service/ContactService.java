0570b22464d182dad8b17bf5a4f87301a957897c
singh2236 <nav.kang4@gmail.com>
2022-10-03 18:00:21 +0200

    Demo of Spring Bean Web Scopes

--- Example_25/src/main/java/com/navi/modelSchool/service/ContactService.java
+++ Example_25/src/main/java/com/navi/modelSchool/service/ContactService.java
@@ -1,21 +1,45 @@
 package com.navi.modelSchool.service;
 
+
 import com.navi.modelSchool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
 
+/*
+@Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
+Logger static property in the class at compilation time.
+* */
 @Slf4j
 @Service
+//@RequestScope
+//@SessionScope
+@ApplicationScope
 public class ContactService {
-    //public static Logger log = LoggerFactory.getLogger(ContactService.class);
-    public boolean saveContactData(Contact contact){
+
+    private int counter = 0;
+
+    public ContactService(){
+        System.out.println("Contact Service Bean initialized");
+    }
+
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+    public boolean saveMessageDetails(Contact contact){
         boolean isSaved = true;
-        //TODO: Need to Persist the Data inti the DB table.
+        //TODO - Need to persist the data into the DB table
         log.info(contact.toString());
         return isSaved;
     }
 
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
 }
