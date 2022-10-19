3c34f1e2bc04d3f09883d734d11c8d37ac6cae56
singh2236 <nav.kang4@gmail.com>
2022-10-19 21:53:29 +0200

    Password hashing using BCryptPasswordEncoder

--- Example_40/src/main/java/com/navi/modelSchool/controller/ProfileController.java
+++ Example_40/src/main/java/com/navi/modelSchool/controller/ProfileController.java
@@ -20,22 +20,17 @@ import javax.validation.Valid;
 @Slf4j
 @Controller
 public class ProfileController {
+
     @Autowired
     PersonRepository personRepository;
 
     @RequestMapping("/displayProfile")
-    public ModelAndView displayMessages(Model model, HttpSession httpSession) {
-        // getting person object from the HttpSession
-        Person person = (Person) httpSession.getAttribute("loggedInPerson");
-        //Instantiating profile object to sent trough ModelAndView to profile.html
+    public ModelAndView displayMessages(Model model, HttpSession session) {
+        Person person = (Person) session.getAttribute("loggedInPerson");
         Profile profile = new Profile();
-
-        //populating the profile fields from the data we get from the person object
         profile.setName(person.getName());
         profile.setMobileNumber(person.getMobileNumber());
         profile.setEmail(person.getEmail());
-
-        //checking if the address values are populated in the Db if yes, we are also showing those to the user.
         if (person.getAddress() != null && person.getAddress().getAddressId() > 0) {
             profile.setAddress1(person.getAddress().getAddress1());
             profile.setAddress2(person.getAddress().getAddress2());
@@ -43,38 +38,30 @@ public class ProfileController {
             profile.setState(person.getAddress().getState());
             profile.setZipCode(person.getAddress().getZipCode());
         }
-
-        // Instantiating ModelAndView to be returned through this method
         ModelAndView modelAndView = new ModelAndView("profile.html");
-
-        // adding an object of type profile to the fround end
         modelAndView.addObject("profile", profile);
         return modelAndView;
     }
 
-    @PostMapping("/updateProfile")
-    public String updateProfile(@Valid @ModelAttribute("profile") Profile profile, Errors errors, HttpSession httpSession) {
+    @PostMapping(value = "/updateProfile")
+    public String updateProfile(@Valid @ModelAttribute("profile") Profile profile, Errors errors, HttpSession session) {
         if (errors.hasErrors()) {
             return "profile.html";
         }
-        Person person = (Person) httpSession.getAttribute("loggedInPerson");
+        Person person = (Person) session.getAttribute("loggedInPerson");
         person.setName(profile.getName());
         person.setEmail(profile.getEmail());
         person.setMobileNumber(profile.getMobileNumber());
-
-        if (person.getAddress() != null && person.getAddress().getAddressId() > 0) {
+        if (person.getAddress() == null || !(person.getAddress().getAddressId() > 0)) {
             person.setAddress(new Address());
-
         }
         person.getAddress().setAddress1(profile.getAddress1());
         person.getAddress().setAddress2(profile.getAddress2());
         person.getAddress().setCity(profile.getCity());
         person.getAddress().setState(profile.getState());
         person.getAddress().setZipCode(profile.getZipCode());
-
-        personRepository.save(person);
-
-        httpSession.setAttribute("loggedInPerson", person);
-        return "redirect/:displayProfile";
+        Person savedPerson = personRepository.save(person);
+        session.setAttribute("loggedInPerson", savedPerson);
+        return "redirect:/displayProfile";
     }
 }
